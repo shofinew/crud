c1d946ace0d046f2c77f1eaeaf8124edb1f782f8
shofinew <shofiullasafayat@gmail.com>
2024-10-14 23:53:28 +0600

    this is column

--- newColumn.php
+++ newColumn.php
@@ -1,4 +1,4 @@
-<!-- <?php
+<?php
 $con = mysqli_connect("localhost", "root", "", "cruddb");
 
 $sql= "ALTER TABLE crud ADD COLUMN `TIME` VARCHAR(255)";
@@ -6,4 +6,4 @@ $ex= mysqli_query($con, $sql);
 if(!$ex){
       die(mysqli_error($con));
 }
-?> -->
+?>
